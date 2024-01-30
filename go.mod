380a1087c9768efb3bb91f22a8f609556b8c554f
Roman Timashev <roman@tmshv.ru>
2024-01-31 02:49:08 +0300

    add initial code for just print all starred repos

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tmshv/ghstars
+
+go 1.21.6
+
+require github.com/joho/godotenv v1.5.1
---
+++ set/set.go
@@ -0,0 +1,30 @@
+package set
+
+type Set[T comparable] struct {
+	elements map[T]bool
+}
+
+func New[T comparable]() *Set[T] {
+	return &Set[T]{elements: make(map[T]bool)}
+}
+
+func (s *Set[T]) Add(value T) {
+	s.elements[value] = true
+}
+
+func (s *Set[T]) Del(value T) {
+	delete(s.elements, value)
+}
+
+func (s *Set[T]) Contains(value T) bool {
+	_, exists := s.elements[value]
+	return exists
+}
+
+func (s *Set[T]) Items() []T {
+	items := make([]T, 0, len(s.elements))
+	for value := range s.elements {
+		items = append(items, value)
+	}
+	return items
+}
